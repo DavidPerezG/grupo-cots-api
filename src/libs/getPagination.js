e5dd1ce5750138f12f13f8479c5b8807ac6e4030
DavidPerezG <davidperezg72001@outlook.com>
2021-09-18 01:57:34 -0700

    size 20

--- src/libs/getPagination.js
+++ src/libs/getPagination.js
@@ -1,6 +1,6 @@
 
 export const getPagination = (page, size) => {
-    const limit =size ? +size : 3;
+    const limit =size ? +size : 20;
     const offset = page ? page * limit : 1;
     return { limit, offset}
 }
